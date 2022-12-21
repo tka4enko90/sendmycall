2a1cdd55df8150138853b9bd5c23a81dc0a634ad
Serhii Skichko <sskichko@markupus.tech>
2022-12-21 13:54:08 +0200

    contact us page

--- wp-content/themes/sendmycall/header.php
+++ wp-content/themes/sendmycall/header.php
@@ -15,6 +15,7 @@ $buy_now = get_field( 'header_buy_now', 'options' );
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title><?php the_title(); ?></title>
 	<?php
 	wp_head();
 	?>
