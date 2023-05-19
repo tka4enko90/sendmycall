8a9fc5a0165a62a5a535a08030be5ebd2699e9c8
Serhii Skichko <sskichko@markupus.tech>
2023-05-19 16:38:01 +0300

    style blog, single post

--- wp-content/themes/sendmycall/home.php
+++ wp-content/themes/sendmycall/home.php
@@ -7,36 +7,41 @@ get_header();  ?>
 
 <main class="main">
     <div class="container">
-        <?php
-        if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <article class="post">
-            <a href="<?php echo get_permalink(get_the_ID());?>">
-                <?php if ( has_post_thumbnail() ) : ?>
-                    <div class="featured-image">
-                        <?php the_post_thumbnail(); ?>
-                    </div>
-                <?php endif; ?>
-                <h4 class="entry-title"><?php the_title(); ?></h4>
-            </a>
+        <div class="blog">
+            <h3 class="blog_title"> <?php _e( 'Blog', 'sendmycall' ); ?></h3>
+            <div class="blog_posts">
+                <?php
+                if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    <article class="blog_post">
+                        <a href="<?php echo get_permalink(get_the_ID());?>">
+                            <?php if ( has_post_thumbnail() ) : ?>
+                                <div class="blog_image">
+                                    <?php the_post_thumbnail(); ?>
+                                </div>
+                            <?php endif; ?>
+                            <h4 class="blog_post_title"><?php the_title(); ?></h4>
+                        </a>
 
-            <div class="entry-meta">
-                <span class="posted-on">
-                    <?php _e( 'Posted on', 'sendmycall' ); ?>
-                    <a href="<?php the_permalink(); ?>"><?php the_time( get_option( 'date_format' ) ); ?></a>
-                </span>
-            </div>
+                        <div class="blog_post_meta">
+                            <span class="blog_post_meta_posted_on">
+                                <?php _e( 'Posted on:', 'sendmycall' ); ?>
+                                <span class="blog_post_meta_date"><?php the_time( get_option( 'date_format' ) ); ?></span>
+                            </span>
+                        </div>
 
-            <div class="entry-content">
-                <?php the_content(); ?>
-            </div>
+                        <div class="blog_post_content">
+                            <?php the_content(); ?>
+                        </div>
 
-            <div class="comments">
-                <?php if ( comments_open() || get_comments_number() ) :
-                    comments_template();
-                endif; ?>
+                        <div class="blog_post_comments">
+                            <?php if ( comments_open() || get_comments_number() ) :
+                                comments_template();
+                            endif; ?>
+                        </div>
+                    </article>
+                <?php endwhile; endif; ?>
             </div>
-        </article>
-    <?php endwhile; endif; ?>
+        </div>
     </div>
 </main>
 <?php
--- wp-content/themes/sendmycall/single-post.php
+++ wp-content/themes/sendmycall/single-post.php
@@ -3,28 +3,30 @@
  * @package WordPress
  * @subpackage sendmycall
  */
+if ( function_exists( 'wp_enqueue_style' ) ) {
+    wp_enqueue_style( 'blog', get_template_directory_uri() . '/dist/css/single-post.css', '', '', 'all' );
+}
+
 get_header();
 
 ?>
 
 <main class="main">
     <div class="container">
-        <?php
-            while ( have_posts() ) :
-                the_post(); ?>
-
-            <h3><?php the_title(); ?></h3>
-            <?php if ( has_post_thumbnail() ) : ?>
-                <div class="featured-image">
-                    <?php the_post_thumbnail(); ?>
-                </div>
-            <?php endif; ?>
-            <div class="entry-content">
-                <?php the_content(); ?>
-            </div>
+        <div class="post_holder">
             <?php
-            endwhile;
-        ?>
+                if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    <h3 class="post_title"><?php the_title(); ?></h3>
+                    <?php if ( has_post_thumbnail() ) : ?>
+                        <div class="post_image">
+                            <?php the_post_thumbnail(); ?>
+                        </div>
+                    <?php endif; ?>
+                    <div class="post_content">
+                        <?php the_content(); ?>
+                    </div>
+            <?php endwhile; endif;?>
+        </div>
     </div>
 </main>
 
